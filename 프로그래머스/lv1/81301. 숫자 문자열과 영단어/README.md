d3ecca1efd2fe2c1a3f52d3438e35c2a29838124
saypart <54827741+saypart@users.noreply.github.com>
2022-12-19 10:35:23 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.45 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=java) 
 
 ### 성능 요약
 
-메모리: 80.1 MB, 시간: 1.51 ms
+메모리: 77.3 MB, 시간: 0.45 ms
 
 ### 구분
 
--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,60 +2,15 @@ import java.util.Objects;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
-               String[] str = s.split("");
-        String ant = "";
-        for(int i = 0; i< s.length(); i++){
-            if(Objects.equals(str[i], "z")) {
-                i = i + 3;
-                ant = ant + "0";
-            }
-            else if(Objects.equals(str[i], "o")) {
-                i = i + 2;
-                ant = ant + "1";
-            }
-            else if(Objects.equals(str[i], "t")) {
-               if(Objects.equals(str[i+1], "w")) {
-                   i = i + 2;
-                   ant = ant + "2";
-               }
-                if(Objects.equals(str[i+1], "h")) {
-                    i = i + 4;
-                    ant = ant + "3";
-                }
-            }
-            else if(Objects.equals(str[i], "f")) {
-                if(Objects.equals(str[i+1], "o")) {
-                    i = i + 3;
-                    ant = ant + "4";
-                }
-                if(Objects.equals(str[i+1], "i")) {
-                    i = i + 3;
-                    ant = ant + "5";
-                }
-            }
-            else if(Objects.equals(str[i], "s")) {
-                if(Objects.equals(str[i+1], "i")) {
-                    i = i + 2;
-                    ant = ant + "6";
-                }
-                if(Objects.equals(str[i+1], "e")) {
-                    i = i + 4;
-                    ant = ant + "7";
-                }
-            }
-            else if(Objects.equals(str[i], "e")) {
-                i = i + 4;
-                ant = ant + "8";
-            }
+       int answer = 0;
+        int len = s.length();
+        String[] digits = {"0","1","2","3","4","5","6","7","8","9"};
+        String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
 
-            else if(Objects.equals(str[i], "n")) {
-                i = i + 3;
-                ant = ant + "9";
-            }
-            else {ant = ant+str[i];}
+        for(int i=0; i<10; i++){
+            s = s.replaceAll(alphabets[i],digits[i]);
         }
-        answer = Integer.parseInt(ant);
-        return answer;
+
+        return Integer.parseInt(s);
     }
 }
