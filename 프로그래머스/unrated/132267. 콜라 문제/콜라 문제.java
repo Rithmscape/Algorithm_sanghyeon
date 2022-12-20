a045c7eb2cdecc455a04e53f24c901365daa1164
saypart <54827741+saypart@users.noreply.github.com>
2022-12-20 22:53:16 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,5 +1,13 @@
 class Solution {
     public int solution(int a, int b, int n) {
-       return (n > b ? n - b : 0) / (a - b) * b;
+        int answer = 0;
+        int add = 0;
+        while (n>=a & n!=0 ) {
+             add = (n/a)*b ;
+             answer = answer+add;
+             n = add+(n%a);
+             add = 0;
+        }
+        return answer;
     }
 }
