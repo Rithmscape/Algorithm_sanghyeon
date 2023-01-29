e1fa9ed6add5e9a4754fe4239f28a9093e1ed160
saypart <54827741+saypart@users.noreply.github.com>
2023-01-30 04:16:46 +0900

    0130 (#19)
    * [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 80.3 MB -BaekjoonHub
    
    * [level 1] Title: 숫자 문자열과 영단어, Time: 1.51 ms, Memory: 80.1 MB -BaekjoonHub
    
    * [level 1] Title: 숫자 문자열과 영단어, Time: 0.45 ms, Memory: 77.3 MB -BaekjoonHub
    
    * [Bronze I] Title: 단어 공부, Time: 2028 ms, Memory: 119364 KB -BaekjoonHub
    
    * [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub
    
    * [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 75.4 MB -BaekjoonHub
    
    * [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 72.8 MB -BaekjoonHub
    
    * [unrated] Title: 명예의 전당 (1), Time: 0.31 ms, Memory: 72.7 MB -BaekjoonHub
    
    * [level 1] Title: 크레인 인형뽑기 게임, Time: 0.87 ms, Memory: 76 MB -BaekjoonHub
    
    * [level 1] Title: 완주하지 못한 선수, Time: 197.86 ms, Memory: 96.3 MB -BaekjoonHub
    
    * [level 2] Title: 가장 큰 수, Time: 1.74 ms, Memory: 72.8 MB -BaekjoonHub
    
    * [level 2] Title: 가장 큰 수, Time: 1.74 ms, Memory: 72.8 MB -BaekjoonHub
    
    * [level 2] Title: 가장 큰 수, Time: 1.67 ms, Memory: 80 MB -BaekjoonHub
    
    * [level 2] Title: 가장 큰 수, Time: 1.67 ms, Memory: 80 MB -BaekjoonHub
    
    * [level 1] Title: [1차] 비밀지도, Time: 6.33 ms, Memory: 76.9 MB -BaekjoonHub
    
    * [level 1] Title: [1차] 비밀지도, Time: 7.34 ms, Memory: 78 MB -BaekjoonHub
    
    * [level 1] Title: 부족한 금액 계산하기, Time: 0.09 ms, Memory: 75.1 MB -BaekjoonHub
    
    * [unrated] Title: 인기있는 아이스크림, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub
    
    * [level 1] Title: 강원도에 위치한 생산공장 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub
    
    * [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub
    
    * [level 1] Title: 신고 결과 받기, Time: 1.04 ms, Memory: 75.7 MB -BaekjoonHub
    
    * [level 1] Title: 신규 아이디 추천, Time: 1.32 ms, Memory: 65.7 MB -BaekjoonHub
    
    * [level 1] Title: 이상한 문자 만들기, Time: 1.63 ms, Memory: 78.8 MB -BaekjoonHub
    
    * [level 1] Title: 3진법 뒤집기, Time: 0.11 ms, Memory: 82.1 MB -BaekjoonHub
    
    * [level 2] Title: 이진 변환 반복하기, Time: 22.28 ms, Memory: 86.9 MB -BaekjoonHub
    
    * [level 2] Title: 위장, Time: 0.06 ms, Memory: 75.8 MB -BaekjoonHub
    
    * [level 2] Title: 피로도, Time: 0.48 ms, Memory: 78.6 MB -BaekjoonHub
    
    * [unrated] Title: 개인정보 수집 유효기간, Time: 2.25 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,45 @@
+import java.time.LocalDate;
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        HashMap<String,Integer> clause = new HashMap<>();
+
+        for(int i = 0;i <terms.length;i++){
+            String[] buffer  = terms[i].split(" ");
+            clause.put(buffer[0], Integer.valueOf(buffer[1]));
+        }
+        LocalDate[] dataList = new LocalDate[privacies.length];
+        String[][] user = new String[privacies.length][2];
+        for (int j = 0;j <privacies.length;j++){
+            String[] buffer  = privacies[j].split(" ");
+            user[j][0]  = buffer[0];
+            user[j][1]  = buffer[1];
+            int add = clause.get(buffer[1]);
+            int[] bufferDay  = {add/12,add%12} ;
+            String[] dayBuffer = buffer[0].split("\\.");
+            if(Integer.parseInt(dayBuffer[1])+bufferDay[1] > 12){
+                bufferDay[0] = bufferDay[0]+1;
+                bufferDay[1] = bufferDay[1]-12;
+            }
+            dataList[j] = LocalDate.of(Integer.parseInt(dayBuffer[0])+bufferDay[0],Integer.parseInt(dayBuffer[1])+bufferDay[1],Integer.parseInt(dayBuffer[2]));
+        }
+
+        String[] todayBuffer = today.split("\\.");
+        LocalDate localDay = LocalDate.of(Integer.parseInt(todayBuffer[0]),Integer.parseInt(todayBuffer[1]),Integer.parseInt(todayBuffer[2]));
+
+        Queue<Integer>count = new LinkedList<>();
+        for(int k = 0; k<dataList.length;k++){
+            if(localDay.isAfter(dataList[k])|localDay.equals(dataList[k])){
+              count.add(k+1);
+            }
+        }
+        int size = count.size();
+        int[] answer = new int[count.size()];
+        for (int l = 0; l < size;l++ ){
+            answer[l] = count.poll();
+        }
+
+        return answer;
+    }
+}
