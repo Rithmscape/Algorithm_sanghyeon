a6f1279cfaf7118d5b00db8922e7f4d6a962566c
saypart <54827741+saypart@users.noreply.github.com>
2023-02-20 04:15:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.15 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,39 +1,6 @@
-import java.util.Queue;
-import java.util.LinkedList;
 class Solution {
     public int solution(int[][] maps) {
-         int[][] check = new int[maps.length][maps[0].length];
-
-        check[0][0] = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
-
-
-        while (!queue.isEmpty()){
-            int[] point = queue.poll();
-
-            if(point[0]== maps.length-1&&point[1]==maps[0].length-1){
-                return check[maps.length-1][maps[0].length-1];
-            }
-
-            if(point[0]+1 < maps.length && maps[point[0]+1][point[1]] == 1 && check[point[0]+1][point[1]] == 0) {
-                queue.add(new int[] {point[0]+1, point[1]});
-                check[point[0]+1][point[1]] += check[point[0]][point[1]] + 1;
-            }
-            if(point[0] > 0 && maps[point[0]-1][point[1]] == 1 && check[point[0]-1][point[1]] == 0) {
-                queue.add(new int[] {point[0]-1, point[1]});
-                check[point[0]-1][point[1]] += check[point[0]][point[1]] + 1;
-            }
-            if(point[1]+1 < maps[0].length && maps[point[0]][point[1]+1] == 1 && check[point[0]][point[1]+1] == 0) {
-                queue.add(new int[] {point[0], point[1]+1});
-                check[point[0]][point[1]+1] += check[point[0]][point[1]] + 1;
-            }
-            if(point[1] > 0 && maps[point[0]][point[1]-1] == 1 && check[point[0]][point[1]-1] == 0) {
-                queue.add(new int[] {point[0], point[1]-1});
-                check[point[0]][point[1]-1] += check[point[0]][point[1]] + 1;
-            }
-        }
-
-        return -1;
+        int answer = 0;
+        return answer;
     }
 }
