3d50625cb3a7131b45492e1d46a4a33e2c0b8b03
saypart <54827741+saypart@users.noreply.github.com>
2023-03-10 03:59:55 +0900

    [level 2] Title: 조이스틱, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -1,21 +1,17 @@
 class Solution {
     public int solution(String name) {
-        int answer = 0;
-        int length = name.length();
-
-        int index;
-        int move = length - 1;
-
-        for(int i = 0; i < name.length(); i++){
+         int answer = 0;
+        int move = name.length() - 1;
+        for (int i = 0; i <name.length();i++){
             answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
 
-            index = i + 1;
-            while(index < length && name.charAt(index) == 'A'){
+            int index = i + 1;
+            while(index < name.length() && name.charAt(index) == 'A'){
                 index++;
             }
-            move = Math.min(move, i * 2 + length - index);
-            move = Math.min(move, (length - index) * 2 + i);
+            move = Math.min(move, i * 2 + name.length() - index);
+            move = Math.min(move, (name.length() - index) * 2 + i);
         }
-        return answer + move;
+        return answer+move;
     }
 }
